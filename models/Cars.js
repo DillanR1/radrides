702f53fc01f805c13ab005c73cd9f3fd64842f1b
Dillan <dillanroby@yahoo.com>
2020-08-23 17:59:42 -0700

    added generic car data no URL yet

--- models/Cars.js
+++ models/Cars.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CarSchema = new Schema({
-    title: String,
     year: Number,
     make: String,
     model: String,
+    carPicUrl: String,
 });
 
 const Car = mongoose.model('Car', CarSchema);
